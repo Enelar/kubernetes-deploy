d43ece9473334251e1dc93fbcd180b641c2d87f3
Zenara Daley <zenara.daley@shopify.com>
2018-02-16 14:17:04 -0500

    Updated mail resource

--- lib/kubernetes-deploy/kubernetes_resource/mail.rb
+++ lib/kubernetes-deploy/kubernetes_resource/mail.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 module KubernetesDeploy
   class Mail < KubernetesResource
+    GROUP = 'stable.shopify.io'
+    PREDEPLOY = true
     TIMEOUT = 30.seconds
+    VERSION = 'v1'
 
     def sync
       _, _err, st = kubectl.run("get", type, @name)
